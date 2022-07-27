b5ca78dca9dd77be878b32f1fcd757ac7a1c31ef
Rabia Yada <61634249+Rabia-YADA@users.noreply.github.com>
2022-07-27 12:45:53 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
-import { Photo } from "./entity/Photo"
-import { SignUp } from "./entity/SignUp"
 
 
 export const AppDataSource = new DataSource({
